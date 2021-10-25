5561d73db033ae95a6fc6c9d378fcec147dbdb2e
Davi Cruz <dacruz@microsoft.com>
2021-10-25 11:01:16 -0300

    Fix to Logic App

--- Sentinel/logic-integration-entityToMarkdown/logic-integration-entityToMarkdown.js
+++ Sentinel/logic-integration-entityToMarkdown/logic-integration-entityToMarkdown.js
@@ -9,68 +9,64 @@
 // Logic App 
 let objJSON = workflowContext.trigger.outputs.body.object.properties.relatedEntities;
 
-function tableMD() {
-    // List entity Types
-    var arrKind = [];
-    objJSON.forEach(objEntity => {
-        arrKind.push(objEntity.kind);
+// List entity Types
+var arrKind = [];
+objJSON.forEach(objEntity => {
+    arrKind.push(objEntity.kind);
+});
+var setKind = new Set(arrKind);
+
+// Dynamically builds entity tables by kind
+var result = ["### Entities", ""];
+setKind.forEach(strKind => {
+    result.push("");
+    result.push("#### Entity Type: **" + strKind + "**");
+    result.push("");
+    scope = objJSON.filter(entity => entity.kind == strKind);
+
+    // List all key values forspecified entity kind
+    keys = [];
+    scope.forEach(entity => {
+        Object.keys(entity.properties).forEach(key => {
+            keys.push(key);
+        });
     });
-    var setKind = new Set(arrKind);
 
-    // Dynamically builds entity tables by kind
-    var result = ["### Entities", ""];
-    setKind.forEach(strKind => {
-        result.push("");
-        result.push("#### Entity Type: **" + strKind + "**");
-        result.push("");
-        scope = objJSON.filter(entity => entity.kind == strKind);
+    // Builds markdown header
+    tableHeader = [];
+    tableDelimiter = [];
+    new Set(keys).forEach(key => {
+        tableHeader.push(key);
+        tableDelimiter.push("-");
+    });
+    result.push('| ' + tableHeader.join(" | ") + " |");
+    result.push('| ' + tableDelimiter.join(" | ") + " |");
 
-        // List all key values forspecified entity kind
-        keys = [];
-        scope.forEach(entity => {
-            Object.keys(entity.properties).forEach(key => {
-                keys.push(key);
-            });
-        });
-
-        // Builds markdown header
-        tableHeader = [];
-        tableDelimiter = [];
-        new Set(keys).forEach(key => {
-            tableHeader.push(key);
-            tableDelimiter.push("-");
-        });
-        result.push('| ' + tableHeader.join(" | ") + " |");
-        result.push('| ' + tableDelimiter.join(" | ") + " |");
-
-        // Populate table with available data
-        scope.forEach(entity => {
-            tableRow = [];
-            keys.forEach(key => {
-                if (entity.properties[key]) {
-                    if (typeof entity.properties[key] == 'object') {
-                        // Stringfy objects fields
-                        tableRow.push(JSON.stringify(entity.properties[key]));
-                    }
-                    else {
-                        tableRow.push(entity.properties[key]);
-                    }
+    // Populate table with available data
+    scope.forEach(entity => {
+        tableRow = [];
+        keys.forEach(key => {
+            if (entity.properties[key]) {
+                if (typeof entity.properties[key] == 'object') {
+                    // Stringfy objects fields
+                    tableRow.push(JSON.stringify(entity.properties[key]));
                 }
                 else {
-                    tableRow.push("");
+                    tableRow.push(entity.properties[key]);
                 }
-            });
-            // Append line to result array
-            result.push('| ' + tableRow.join(" | ") + " |");
+            }
+            else {
+                tableRow.push("");
+            }
         });
+        // Append line to result array
+        result.push('| ' + tableRow.join(" | ") + " |");
     });
+});
 
-    // Concatenate array in a single string
-    output = result.join("\n");
+// Concatenate array in a single string
+output = result.join("\n");
 
-    // Test
-    //console.log(output);
-    return output;
-}
-
-tableMD(objJSON);
+// Test
+//console.log(output);
+return output;
