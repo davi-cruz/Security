e392504404f9542ecf30b150d7bcc1a725024e9e
Davi Cruz <eu@davicruz.com>
2023-06-23 18:59:06 -0300

    MDE Windows

---
+++ MDE/README.md
@@ -0,0 +1,9 @@
+# Microsoft Defender for Endpoint (MDE) Resources
+
+## Linux
+
+- [Ansible Playbooks for MDE](https://github.com/davi-cruz/Security/tree/main/MDE/Linux/AnsiblePlaybooks): Playbooks for installing and configuring MDE on Linux systems. As of today it also contains a playbook for policy management.
+
+## Windows
+
+- [`Get-MDELogs.ps1`](https://github.com/davi-cruz/Security/tree/main/MDE/Windows/Get-MDELogs.ps1): Script to collect MDE logs from a Windows system and save them in a zip file for a quick analysis when MDE Client Analyzer is too much for the job.
---
+++ MDE/Windows/Get-MDELogs.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    Collects MDE Computer Status and Preferences, as well as MDM Logs, in order to make it simple to review
+    currently applied settings either manually or via MDM.
+    Output file defaults to C:\Temp\MDESettings-<ComputerName>-<DateTime>.zip, but can be changed via the OutputFolder parameter.
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [string]$OutputFile = "C:\Temp\MDESettings-$($env:COMPUTERNAME)-$(Get-Date -f "yyyyMMddHHmmss").zip"
+)
+
+$folder = $OutputFile.Replace(".zip", "")
+
+# Prepare results folder (which will be later removed)
+New-Item -Path $folder -ItemType Directory -Force | Out-Null
+
+# Collect MDE Settings
+$commands = @("Get-MpComputerStatus", "Get-MpPreference")
+
+foreach ($command in $commands) {
+    Invoke-Expression $command | Select-Object -ExcludeProperty CimClass, CimSystemProperties | ConvertTo-Json -Depth 5 | Out-File -FilePath "$folder\$command.json"
+}
+
+# Collect MDM Settings
+&"C:\Windows\System32\MdmDiagnosticsTool.exe" -out "$folder"
+
+# Compress results and save to output file
+Compress-Archive -Path $folder -DestinationPath "$folder.zip"
+
+# Cleanup temporary folder
+Remove-Item -Path $folder -Recurse -Force
