2e578c1881325fe00b093ae76d66f9bbab7be54e
Davi Cruz <eu@davicruz.com>
2023-06-26 12:38:38 -0300

    Included Intune Backup for MDE Evaluation on Windows

--- MDE/README.md
+++ MDE/README.md
@@ -7,3 +7,6 @@
 ## Windows
 
 - [`Get-MDELogs.ps1`](https://github.com/davi-cruz/Security/tree/main/MDE/Windows/Get-MDELogs.ps1): Script to collect MDE logs from a Windows system and save them in a zip file for a quick analysis when MDE Client Analyzer is too much for the job.
+
+- [MDE Intune Evaluation Config](https://github.com/davi-cruz/Security/tree/main/MDE/Windows/IntuneEvaluationConfig): Sample Intune Policies to evaluate MDE Capabilities based on article [Evaluate Microsoft Defender Antivirus](https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/evaluate-microsoft-defender-antivirus?view=o365-worldwide).
+  - Usage: Download assets and run `.\Import-IntunePolicies.ps1` point to the folder where you downloaded the assets. Files must be in the folder structure used by IntuneBackupAndRestore Module.
---
+++ MDE/Windows/IntuneEvaluationConfig/Import-MDEIntuneEvaluationConfig.ps1
@@ -0,0 +1,40 @@
+<#
+This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.
+THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. 
+
+We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object
+code form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to market Your software
+product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which the
+Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims
+or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code.
+#>
+[CmdletBinding()]
+param (
+    [string]$BackupPath = "C:\Temp\BackupIntune\"
+)
+
+$modules = @("IntuneBackupAndRestore", "Microsoft.Graph.Intune")
+foreach($module in $modules) {
+    ## Tenta atualizar módulo, se não existir, instala
+    try{
+        Update-module $module -force
+    }
+    catch{
+        Install-Module $module -Scope CurrentUser -force
+    }
+}
+
+## Conecta no Microsoft Graph
+Connect-MSGraph
+
+## Restaura o backup
+## Ajustar o caminho do arquivo de backup
+## Exemplo de estrutura utilizada:
+## tree C:\Temp\BackupIntune
+##├── BackupIntune
+##│   └── Settings Catalog
+##│       ├── MDE-PoC-Antivirus.json
+##│       └── MDE-PoC-AttackSurfaceReductionRules.json
+
+Invoke-IntuneRestoreConfigurationPolicy -Path $BackupPath -Verbose
